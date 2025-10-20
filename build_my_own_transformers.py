86fe82501f0baeafc3a75603abfeb72932e2eef9
Anna <regenelee.miao@gmail.com>
2025-10-19 21:58:15 -0700

    Add the bigram model and some TODO

--- build_my_own_transformers.py
+++ build_my_own_transformers.py
@@ -113,46 +113,47 @@ xb, yb = get_batch(train_data)
 xb[0] # tensor([61,  1, 45, 53, 47, 52, 45,  1], device='mps:0')
 print(decoder(xb[0])) # "w going "
 
-# Self Attention
-# now we move on to Self attention with K, Q, V
-# let's use cosine similarity of k and q to represent weights, for just xb0
-# weights means: how much interest do I care about other tokens (before me, here)
-# Callouts: all batches share the same weights matrix (do they?)
+# The Simple Bigram Model
+# p(next_token|current_token): each token is predicted from the token ID before it
 class BigramLanguageModel(nn.Module):
-
     def __init__(self, vocab_size):
         super().__init__()
-        # each token directly reads off the logits for the next token from a lookup table
+        # The embedding table is the target and only output of the Bigram model
+        # Embedding table is a lookup table for indexing,
+        # each row correspond to 1 token ID,
+        # each row vector is the logits(probability distribution) for the next token
+        # aka, table[i, j] = p1 means: when current token is i, the probability of the next token being j is p1
+        # sum(table[i,j], j = 1 to vocab_size) = 1
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
     def forward(self, idx, targets=None):
-
-        # idx and targets are both (B,T) tensor of integers
-        logits = self.token_embedding_table(idx) # (B,T,C)
-
+        # TODO: verify idx is the index of current token that is passed into the model
+        # TODO: what is the logits?
+        logits = self.token_embedding_table(idx)
         if targets is None:
             loss = None
         else:
             B, T, C = logits.shape
             logits = logits.view(B*T, C)
             targets = targets.view(B*T)
-            loss = F.cross_entropy(logits, targets)
-
+            loss = F.cross_entropy(targets, logits)
         return logits, loss
 
     def generate(self, idx, max_new_tokens):
+        # sample from the multinomial probability distributions of the next token of each current token
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
-            # get the predictions
-            logits, loss = self(idx)
-            # focus only on the last time step
-            logits = logits[:, -1, :] # becomes (B, C)
-            # apply softmax to get probabilities
-            probs = F.softmax(logits, dim=-1) # (B, C)
-            # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
-            # append sampled index to the running sequence
-            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+            # get the predictions and loss
+            logits, loss = self(idx) # todo: what does self(idx) mean?
+            # focus on only the last token (where T dimention only remains 1 element)
+            logits = logits[:, -1, :] # (B,1,C), aka (B,C) # todo: is C = n_embd = vocab_size here?
+            # Make the logits into multinomial distribution
+            # todo: this is weird, if there's (B,C), then would the softmax takes C vector and softmax by row
+            prob_distribution = F.softmax(logits, dim=-1)  # (B,C)
+            # generate the next token: for each row in B, sample from the distribution saved in row[b]
+            idx_next = torch.multimonial(prob_distribution, num_samples=1) # (B,1)
+            # append the generated next token to the running sequence idx which is (B,T) + (B,1) = (B, T+1)
+            idx = torch.cat((idx, idx_next), dim=1)
         return idx
 
 model = BigramLanguageModel(vocab_size)
@@ -183,10 +184,11 @@ print(decoder(m.generate(context, max_new_tokens=500)[0].tolist()))
 
 
 
-
-
-
-
+# Transformers
+# now we move on to Self attention with K, Q, V
+# let's use cosine similarity of k and q to represent weights, for just xb0
+# weights means: how much interest do I care about other tokens (before me, here)
+# Callouts: all batches share the same weights matrix (do they?)
 
 cuda.empty_cache()
 gc.collect()
